c8e063cc32c233173b29f5e2bc7a52e7c940b8b3
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-10-23 02:59:19 +0530

    👍

---
+++ static/script.js
@@ -0,0 +1,27 @@
+function getFact() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            fact = JSON.parse(this.responseText);
+            document.getElementById("fact-txt").innerHTML = fact["text"];
+            document.getElementById("source_link").setAttribute("href", fact["source_url"]);
+            document.getElementById("fact-btns").style.display = "block";
+            console.log(fact["source_url"])
+        }
+    };
+    xhttp.open("GET", "/getFact", true);
+    xhttp.send();
+}
+
+function share() {
+    console.log("share")
+    var link = document.getElementById("source_link").getAttribute("href");
+    navigator.clipboard.writeText(link);
+    var tooltip = document.getElementById("tipText");
+    tooltip.innerHTML = "Copied";
+
+}
+function outFunc() {
+    var tooltip = document.getElementById("tipText");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
