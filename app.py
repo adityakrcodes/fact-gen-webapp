b8ece647abf54d1d7dd13b410c44c41e4c520e02
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-10-23 01:56:45 +0530

    Basic Functionality DONE

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html', )
+    return render_template('index.html')
 
-@app.route('/gen')
-def gen():
-    print("Hello")
-    return {"result": "success"}
+@app.route('/getFact')
+def getFact():
+    url = 'https://uselessfacts.jsph.pl/api/v2/facts/random'
+    headers = {'Accept': 'application/json'}
+    fact = requests.get(url, headers=headers).json()
+    return fact
 
 if __name__ == '__main__':
     app.run(debug=True)
